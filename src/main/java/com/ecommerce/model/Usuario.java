788f6efbde486c74e70f8d0a3c530991f4314b3f
hashem <ubaqueb@yahoo.com>
2021-11-15 18:06:05 -0500

    Se crear la funcionalidad guardar producto vista create y Logger para la clase ProductoController

--- src/main/java/com/ecommerce/model/Usuario.java
+++ src/main/java/com/ecommerce/model/Usuario.java
@@ -36,6 +36,20 @@ public class Usuario {
 	
 	
 	public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono,
+			String tipo, String password) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.username = username;
+		this.email = email;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.tipo = tipo;
+		this.password = password;
+	}
+
+
+	public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono,
 			String tipo, String password, List<Producto> productos, List<Orden> ordenes) {
 		super();
 		this.id = id;
